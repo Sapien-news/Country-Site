674cabdaf0eccb53dcb14cf6e02a653d71ad91b7
Abhinav Sharma <abhinavsharma2689@gmail.com>
2023-04-05 19:10:12 +0100

    gltf uploaded

--- main.js
+++ main.js
@@ -1,41 +1,27 @@
-// Create the scene
-var scene = new THREE.Scene();
+// Initialize the Three.js library
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById("canvas") });
 
-// Create the camera
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+// Load the GLTF file
+const loader = new THREE.GLTFLoader();
+loader.load("rename.gltf", function (gltf) {
+  // Add the loaded object to the scene
+  scene.add(gltf.scene);
 
-// Create the renderer
-var renderer = new THREE.WebGLRenderer({canvas: document.getElementById("canvas")});
+  // Set the initial position of the camera
+  camera.position.z = 5;
 
-// Create the geometry
-var geometry = new THREE.BoxGeometry();
+  // Animate the scene
+  function animate() {
+    requestAnimationFrame(animate);
 
-// Create the material
-var material = new THREE.MeshBasicMaterial({color: 0xffffff});
+    // Rotate the object as you scroll
+    gltf.scene.rotation.y += window.scrollY * 0.001;
 
-// Create the mesh
-var mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+    // Render the scene
+    renderer.render(scene, camera);
+  }
 
-// Handle window resizing
-window.addEventListener('resize', function() {
-  var width = window.innerWidth;
-  var height = window.innerHeight;
-  renderer.setSize(width, height);
-  camera.aspect = width / height;
-  camera.updateProjectionMatrix();
+  animate();
 });
-
-// Handle scroll event
-window.addEventListener('scroll', function() {
-  var scrollPosition = window.pageYOffset;
-  mesh.rotation.y = scrollPosition / 1000;
-});
-
-// Render the scene
-function render() {
-  requestAnimationFrame(render);
-  renderer.render(scene, camera);
-}
-render();

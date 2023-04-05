8b3b55f0b49fa60b9067d6b98b137ad7c20b2610
Abhinav Sharma <abhinavsharma2689@gmail.com>
2023-04-05 20:17:05 +0100

    text

--- main.js
+++ main.js
@@ -31,22 +31,26 @@ loader.load("scene.glb", function (gltf) {
   animate();
 });
 
-
 // get all the text-line elements
 const textLines = document.querySelectorAll('.text-line');
 
 // create a new Intersection Observer
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      // add the "animate" class to the text-line element
-      entry.target.classList.add('animate');
-      // unobserve the target element
-      observer.unobserve(entry.target);
-    }
-  });
-}, { threshold: 0.1 });
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // add the "animate" class to the text-line element
+        entry.target.classList.add('animate');
+        // unobserve the target element
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    rootMargin: '0px',
+    threshold: 0.1
+  }
+);
 
 // observe each text-line element
 textLines.forEach(line => observer.observe(line));
-

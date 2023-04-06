4837bcb145be8ffdcb0a1fba13a2e99789f5aa65
Abhinav Sharma <abhinavsharma2689@gmail.com>
2023-04-06 01:23:41 +0100

    -3js

--- main.js
+++ main.js
@@ -1,32 +1,3 @@
-// Initialize the Three.js library
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById("canvas") });
-
-// Load the GLTF file
-const loader = new THREE.GLTFLoader();
-
-loader.load("scene.glb", function (gltf) {
-    // Add the loaded object to the scene
-    scene.add(gltf.scene);
-  
-    // Set the initial position of the camera
-    camera.position.set(100, 0, 0); // Adjust the Z value to control the distance from the object
-    camera.lookAt(scene.position);
-  
-    // Animate the scene
-    function animate() {
-      requestAnimationFrame(animate);
-  
-      // Rotate the object continuously
-      gltf.scene.rotation.y += 0.01;
-  
-      // Render the scene
-      renderer.render(scene, camera);
-    }
-  
-    animate();
-});  
 
 // get all the text-line elements
 const textLines = document.querySelectorAll('.text-line');
